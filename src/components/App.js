ab430225bc1ab3227c6ef0128ca0fdd73da657ce
ritikchhimwal <ritikchhimwal822@gmail.com>
2024-12-30 14:00:14 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetail from "./UserDetail";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/users/:id" component={UserDetail} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
+
---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const UserDetail = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        setUser(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching user details:", error);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <p>User not found</p>;
+  }
+
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+    </div>
+  );
+};
+
+export default UserDetail;
